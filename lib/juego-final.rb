06ee75bb0b5bf5ccaba89a46fe509962934c95ed
vagrant <vagrant@kleer-csd-box>
2017-03-15 11:34:09 -0300

    rescate del lib

---
+++ lib/juego-final.rb
@@ -0,0 +1,51 @@
+class Juego
+
+	def initialize
+		@jugador1=nil
+		@jugador2=nil
+		@mostrar="Inicio"
+	end
+
+	def setjugador1 valor
+		@jugador1=valor
+	end
+
+	def setjugador2 valor
+		@jugador2=valor
+	end
+
+	def getjugador1
+		return @jugador1
+	end
+
+	def getjugador2
+		return @jugador2
+	end
+
+
+	def mostrar
+		if @jugador1==nil and @jugador2==nil
+			return @mostrar="Inicio"
+		else
+			if @jugador1==@jugador2
+				return @mostrar="Empate"
+			end
+		end
+
+
+		if (@jugador1=="Y" and @jugador2=="P") or (@jugador1=="K" and @jugador2=="Y") or (@jugador1=="P" and @jugador2=="K")
+			return @mostrar="Gana Jugador 1"
+		end 
+
+
+		if (@jugador1=="P" and @jugador2=="Y") or (@jugador1=="K" and @jugador2=="P") or (@jugador1=="Y" and @jugador2=="K")
+			return @mostrar="Gana Jugador 2"
+		end
+	end
+
+
+
+end
+
+
+
